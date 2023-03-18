ed7e24a36351a4aac84e3327e7968b7fc3ac8a73
BetterCallUgL <drushockek@gmail.com>
2023-03-18 18:44:49 +0300

    lol

--- manager/manager.c
+++ manager/manager.c
@@ -9,6 +9,10 @@
 
 int main() {
     struct node** roots = malloc(sizeof(struct node*));
+    void* context = zmq_ctx_new();
+    void** pushers = malloc(sizeof(void*));
+    void* puller = zmq_socket(context,ZMQ_PULL);
+    zmq_bind(puller, "tcp://localhost:5555");
     int root_count = 1, first_root = 1;
     char path[] = "/home/ugl/OS/worker/worker";
     printmenu();
@@ -21,7 +25,6 @@ int main() {
         // Create New Root
         if (!strcmp(command, "root")) {
             int root_id, create_flag = 0;
-            printf("Enter root id\n");
             scanf("%d", &root_id);
 
             if (first_root) {
@@ -39,12 +42,15 @@ int main() {
             }
 
             if (create_flag) {
+                pushers[root_count - 1] = zmq_socket(context, ZMQ_PUSH);
+                char adress[50];
+                sprintf(adress, "tcp://localhost:%d", 10000 + root_id);
+                zmq_connect(pushers[root_count - 1], adress);
                 int process_id = fork();
                 if (!process_id) {
-                    printf("ok: %d\n", getpid());
                     char id[10];
                     sprintf(id, "%d", root_id);
-                    if (execl(path, path, id, NULL) == -1) {
+                    if (execl(path, path, id, "-1", NULL) == -1) {
                         printf("Root hasn't created\n");
                         return -1;
                     }
@@ -52,36 +58,48 @@ int main() {
             }
         }
 
-        // Create New Node
-        if (!(strcmp(command, "node"))) {
-            int parent_id, new_id;
-            struct node* parent = node_exists(roots,root_count,parent_id);
+        // // Create New Node
+        // if (!(strcmp(command, "node"))) {
+        //     int parent_id, new_id;
+        //     struct node* parent = node_exists(roots, root_count, parent_id);
 
-            printf("Enter parent id\n");
-            scanf("%d", &parent_id);
-            if (parent == NULL){
-                printf("This Parent doesn't exists\n");
-                continue;
-            }
-            printf("Enter new id\n");
-            scanf("%d", &new_id);
-            if (node_exists(roots,root_count,new_id) != NULL){
-                printf("This Node already exists\n");
-                continue;
-            }
+        //     printf("Enter parent id\n");
+        //     scanf("%d", &parent_id);
+        //     if (parent == NULL) {
+        //         printf("This Parent doesn't exists\n");
+        //         continue;
+        //     }
+        //     printf("Enter new id\n");
+        //     scanf("%d", &new_id);
+        //     if (node_exists(roots, root_count, new_id) != NULL) {
+        //         printf("This Node already exists\n");
+        //         continue;
+        //     }
 
-            add_element(parent,new_id);
-            int process_id = fork();
-            char line_id[10];
-            sprintf(line_id,"%d",new_id);
-            if (!process_id) {
-                printf("ok: %d\n", getpid());
-                if (execl("/home/ugl/OS/worker/worker", "/home/ugl/OS/worker/worker", line_id, NULL) ==
-                    -1) {
-                    printf("Node hasn't created");
-                    return -1;
-                }
-            }
+        //     add_element(parent, new_id);
+        //     int process_id = fork();
+        //     char line_id[10];
+        //     sprintf(line_id, "%d", new_id);
+        //     if (!process_id) {
+        //         printf("ok: %d\n", getpid());
+        //         if (execl("/home/ugl/OS/worker/worker", "/home/ugl/OS/worker/worker", line_id, NULL) == -1)
+        //         {
+        //             printf("Node hasn't created");
+        //             return -1;
+        //         }
+        //     }
+        // }
+
+        // zmq_msg_t message;
+        // zmq_msg_init(&message);
+        // char m
+        char message[100];
+        int check = zmq_recv(puller,message,100,ZMQ_DONTWAIT);
+        zmq_sleep(5);
+        if (check == -1) {
+            printf("Мужик чё за хуйня)");
+        } else {
+            printf("ураа я получил:%s\n", message);
         }
     }
 }
--- worker/worker.c
+++ worker/worker.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "zmq.h"
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        printf("lol");
-    } else {
-        FILE* f = fopen("hui.txt", "w");
-        fprintf(f, "hui from %d", (atoi)(argv[1]));
-        fclose(f);
+    if (argc < 2){
+        printf("Error, not enough arguments");
+        return -1;
     }
+    
+    void* context = zmq_ctx_new();
+    void* pusher = zmq_socket(context,ZMQ_PUSH);
+    char adress[100];
+    printf("я люблю говно, а ещё :%d\n",atoi(argv[2]));
+    sprintf(adress,"tcp://localhost:%d",5554);
+    perror("бляха муха");
+    zmq_connect(pusher,adress);
+    // zmq_msg_t message;
+    // zmq_msg_init_size(&message,strlen("пиписька"));
+    // memcpy(zmq_msg_data(&message),"пиписька",strlen("пиписька"));
+    // zmq_msg_send(&message,pusher,0);
+    // zmq_msg_close(&message);
+    zmq_send(pusher,"hui",4,0);
+    perror("lol");
+    zmq_close(pusher);
+    zmq_ctx_destroy(context);
     return 0;
 }
